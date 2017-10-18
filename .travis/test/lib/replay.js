8edd2e70272a54d42d92952a300372f1f7a31ca8
Christian M. Boulanger <info@bibliograph.org>
2017-10-18 08:53:58 +0200

    Expand testing depth

--- .travis/test/lib/replay.js
+++ .travis/test/lib/replay.js
@@ -1,8 +1,11 @@
-const fs = require("fs"),
+const 
+  assert = require('assert'),
+  //assert = require('assert-diff'),
+  fs = require("fs"),
   path = require("path"),
   process = require("process"),
   r2 = require("r2");
-  //assert  = require('assert-diff');
+  
 
 // url is dependent on build environment
 let url;
@@ -25,10 +28,8 @@ async function replay(path) {
   let sessionId = null;
   for (let data of replay_data) {
     let request = data.request;
-    if (request.id > 5) return;
     request.server_data.sessionId = sessionId;
-    console.log(">>>> Request");
-    dump(request);
+
     let result;
     let response = await r2.post(url, { json: request }).text;
     try {
@@ -38,13 +39,37 @@ async function replay(path) {
       return;
     }
     if (result.error) {
+      console.log(">>>> Request");
+      dump(request);      
       throw new Error("Error in response: " + error);
     }
-    //assert.deepEqual(result, data.response, 'Output does not match reference content');
-    console.log("<<<< Response (received)");
-    dump(result);
-    console.log("==== Response (expected)");
-    dump(data.response);
+    
+    // compare received and expected json response
+    let received = result;
+    let expected = data.response;
+
+    // this doesn't work yet:
+    //assert.deepEqual(received, expected, 'Output does not match reference content');
+
+    // just checking structural equality (keys)
+    try {
+      assert.deepEqual(Object.keys(received), Object.keys(expected));
+      assert.deepEqual(Object.keys(received.result), Object.keys(expected.result));
+      if(received.result.messages instanceof Array) {
+        assert.equal(received.result.messages.length, expected.result.messages.length);
+      }
+      assert.deepEqual(Object.keys(received.result.data), Object.keys(expected.result.data));
+    } catch(e) {
+      console.log(">>>> Request");
+      dump(request);
+      console.log("<<<< Response (received)");
+      dump(received);
+      console.log("==== Response (expected)");
+      dump(expected);
+      throw new Error("JSONRPC response does not match expectations.");
+    }
+
+    // adapt sessionId
     let messages = result.result.messages;
     if (
       messages instanceof Array &&
