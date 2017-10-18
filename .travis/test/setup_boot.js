97ff6737670ff51d9d0524fc348b9886a879684e
Christian M. Boulanger <info@bibliograph.org>
2017-10-18 10:49:08 +0200

    Make travis test actually fail

--- .travis/test/setup_boot.js
+++ .travis/test/setup_boot.js
@@ -1,8 +1,10 @@
 /* global describe, it */
-const replay  = require('./lib/replay');
+const 
+  assert = require('assert'),
+  replay = require('./lib/replay');
 
 describe('Bibliograph', () => {
   it('should boot and setup the application', async () => {
-    replay(__dirname + "/setup_boot.json");
+    await replay(__dirname + "/setup_boot.json");
   });
 });
