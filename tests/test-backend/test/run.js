6e8180e7aa54b5aea302ec1012ffc00d2be9b279
Christian M. Boulanger <info@bibliograph.org>
2017-10-20 08:53:27 +0200

    Add a pause between setup and boot tests

--- tests/test-backend/test/run.js
+++ tests/test-backend/test/run.js
@@ -7,9 +7,14 @@ const
 let c9 = (process.env.IP && process.env.PORT);
 
 describe('Bibliograph', async function() {
-  this.timeout(10000);
+  this.timeout(20000);
   if( ! c9 ) it ('should setup the application', async () => {
     await replay(__dirname + "/data/setup.json");
+  });
+  it("should wait 5 seconds to let setup finish", function(done){
+    setTimeout(function() {
+        done();
+    }, 5000);
   });
   it('should boot', async () => {
     await replay(__dirname + "/data/boot.json");
