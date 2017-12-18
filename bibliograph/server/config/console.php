8694319652c1c8eca44916816f92020249205d9c
Christian Boulanger <info@bibliograph.org>
2017-12-19 00:15:50 +0100

    update dependencies and config

--- bibliograph/server/config/console.php
+++ bibliograph/server/config/console.php
@@ -1,6 +1,8 @@
 <?php
+$ini = parse_ini_file ( __DIR__ . "/../../services/config/bibliograph.ini.php", true, INI_SCANNER_RAW );
 return [
     'id' => 'bibliograph-server',
+    'bootstrap' => ['gii'],
     'basePath' => dirname(__DIR__) ,
     'controllerNamespace' => 'bibliograph\controllers',
     'aliases' => [
@@ -9,15 +11,21 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=bibliograph',
+            'dsn' => "{$ini['database']['type']}:host={$ini['database']['host']};dbname={$ini['database']['admindb']}",
             'username' => 'bibliograph',
             'password' => 'bibliograph',
             'charset' => 'utf8',
         ],
     ],
+    'modules' => [
+        'gii' => [
+            'class' => 'yii\gii\Module',
+        ]
+    ],
     'controllerMap' => [
         'migration' => [
             'class' => 'bizley\migration\controllers\MigrationController',
         ],
-    ] 
+    ],
+    'extensions' => require(__DIR__ . '/../vendor/yiisoft/extensions.php'),
 ];
--- bibliograph/server/config/web.php
+++ bibliograph/server/config/web.php
@@ -1,5 +1,6 @@
 <?php
-return [
+$ini = parse_ini_file ( __DIR__ . "/../../services/config/bibliograph.ini.php", true, INI_SCANNER_RAW );
+$config =  [
     'id' => 'bibliograph-server',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'bibliograph\controllers',
@@ -7,5 +8,26 @@ return [
         '@bibliograph' => dirname(__DIR__),
     ],
     'components' => [
-    ]
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => "{$ini['database']['type']}:host={$ini['database']['host']};dbname={$ini['database']['admindb']}",
+            'username' => 'bibliograph',
+            'password' => 'bibliograph',
+            'charset' => 'utf8',
+        ],
+        'request' => [
+            'enableCookieValidation' => true,
+            'enableCsrfValidation' => true,
+            'cookieValidationKey' => 'a1a2a3a3d3d4g5g4hfgfh4g',
+        ],
+    ],
+    'extensions' => require(__DIR__ . '/../vendor/yiisoft/extensions.php'),
 ];
+
+if (YII_ENV_DEV) {
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+return $config;
