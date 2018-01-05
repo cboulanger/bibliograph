ce59e64a9bcdca746b571e867334093f4784cd4e
Christian Boulanger <info@bibliograph.org>
2018-01-05 11:25:01 +0100

    travis...

--- test/setup.sh
+++ test/setup.sh
@@ -2,23 +2,23 @@
 
 #set -o errexit # Exit on error
 
-ps | grep "[p]hp yii serve -t=@app/tests localhost:8080" > /dev/null
+ps | grep "[p]hp yii serve -t=@app/tests 127.0.0.1:8080" > /dev/null
 if [ $? -eq 0 ]; then
   echo "Bibliograph test server is running..."
 else
   echo "Starting Bibliograph test server..."
   pushd ./bibliograph/server > /dev/null
-  php yii serve -t=@app/tests localhost:8080 &
+  php yii serve -t=@app/tests 127.0.0.1:8080 &
   popd > /dev/null
 fi
 
-ps | grep "[p]hp yii serve localhost:8081" > /dev/null
+ps | grep "[p]hp yii serve 127.0.0.1:8081" > /dev/null
 if [ $? -eq 0 ]; then
   echo "Bibliograph 'production' server is running..."
 else
   echo "Starting Bibliograph 'production' server..."
   pushd ./bibliograph/server > /dev/null
-  php yii serve localhost:8081 &
+  php yii serve 127.0.0.1:8081 &
   popd > /dev/null
 fi
 
@@ -29,4 +29,5 @@ php yii migrate/fresh --interactive=0 --db=testdb -p=@app/migrations/schema
 echo "travis_fold:end:migrations"
 popd > /dev/null
 
+npm install
 
