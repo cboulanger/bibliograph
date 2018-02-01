3e7d1560e70330efdc328fcf7712820683307cee
Christian Boulanger <info@bibliograph.org>
2018-02-01 14:46:52 +0100

    Update codeception.sh

--- test/codeception.sh
+++ test/codeception.sh
@@ -5,10 +5,11 @@
 pushd ./src/server > /dev/null
 
 # echo "Running Unit tests..."
-# php vendor/bin/codecept run unit
+php vendor/bin/codecept run unit
 
 echo "Running functional tests.."
-php vendor/bin/codecept run functional ReferenceControllerCest --debug
+#php vendor/bin/codecept run functional ReferenceControllerCest --debug
+php vendor/bin/codecept run functional
 
 popd > /dev/null
 
