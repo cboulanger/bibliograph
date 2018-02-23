e44fba2922dc2a0f33d82b5eae1c790d03e0c973
Christian Boulanger <info@bibliograph.org>
2018-02-23 22:58:14 +0100

    All kind of meta files

--- bin/create-migration.sh
+++ bin/create-migration.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+cd src/server
+read -p 'Please enter a short descriptive name of the migration: ' name
+php yii migrate/create --migration-namespaces=app\\migrations\\schema _$(echo $name | sed -e 's/[^a-zA-Z0-9\-]/_/g')
--- bin/view-log.sh
+++ bin/view-log.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+tail -F -n 100 src/server/runtime/logs/app.log
