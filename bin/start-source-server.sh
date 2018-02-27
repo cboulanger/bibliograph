31728ec3986fbf3fe99f1c405c3614558889f2f7
Christian M. Boulanger <info@bibliograph.org>
2018-02-27 10:39:58 +0100

    Update metadata

--- bin/start-source-server.sh
+++ bin/start-source-server.sh
@@ -27,6 +27,8 @@ else
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
+  # assume we have mysql from homebrew
+  mysql.server status 2>&1 | grep SUCCESS || mysql.server start
   echo "Opening Safari browser"
   # Open Safari, better: https://www.npmjs.com/package/webpack-browser-plugin
   open -a Safari http://$HOST/$APP_PATH
