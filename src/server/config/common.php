da7503ba379e8c159181212512be22b0c3d3544b
Christian M. Boulanger <info@bibliograph.org>
2018-04-04 00:00:52 +0200

    Reorganization config files

--- src/server/config/common.php
+++ src/server/config/common.php
@@ -30,7 +30,6 @@ $config =  [
   /* Path aliases */
   'aliases' => [
     '@lib'      => __DIR__ . "/../lib/",
-    '@tests'    => __DIR__ . "/../tests/",
     '@messages' => __DIR__ . "/../messages/"
     ],
 
@@ -41,34 +40,4 @@ $config =  [
   'extensions' => require(__DIR__ . '/../vendor/yiisoft/extensions.php'),
 ];
 
-// Development
-// @todo move into own config file
-if (YII_ENV_DEV) {
-  $config['bootstrap'][] = 'gii';
-  $config['modules']['gii'] = [
-    'class' => 'yii\gii\Module',
-    'generators' => [
-      'fixture' => [
-      'class' => 'elisdn\gii\fixture\Generator',
-      ],
-    ]
-  ];
-  $config['controllerMap'] = array_merge(
-    $config['controllerMap'],[
-    'migrate' => [
-      'class' => 'yii\console\controllers\MigrateController',
-      'migrationNamespaces' => [
-        'app\migrations\schema', 
-        'app\migrations\data'
-      ],
-      'migrationPath' => null
-    ],
-    'migration' => [
-      'class' => 'bizley\migration\controllers\MigrationController'
-    ],
-    'stubs' => [
-      'class' => 'bazilio\stubsgenerator\StubsController',
-    ],    
-  ]);    
-}
 return $config;
---
+++ src/server/config/dev.php
@@ -0,0 +1,30 @@
+<?php
+$config = require "web.php";
+$config['id'] = 'bibliograph-dev-server';
+$config['bootstrap'][] = 'gii';
+$config['modules']['gii'] = [
+  'class' => 'yii\gii\Module',
+  'generators' => [
+    'fixture' => [
+      'class' => 'elisdn\gii\fixture\Generator',
+    ],
+  ]
+];
+$config['controllerMap'] = array_merge(
+  $config['controllerMap'],[
+  'migrate' => [
+    'class' => 'yii\console\controllers\MigrateController',
+    'migrationNamespaces' => [
+      'app\migrations\schema',
+      'app\migrations\data'
+    ],
+    'migrationPath' => null
+  ],
+  'migration' => [
+    'class' => 'bizley\migration\controllers\MigrationController'
+  ],
+  'stubs' => [
+    'class' => 'bazilio\stubsgenerator\StubsController',
+  ],
+]);
+return $config;
