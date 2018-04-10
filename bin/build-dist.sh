8e0a5fbf5e16aa122eb59102d154d61b641cc6a2
Christian M. Boulanger <info@bibliograph.org>
2018-04-10 08:27:15 +0200

    Add release-it tool and start working on travis release system

--- bin/build-dist.sh
+++ bin/build-dist.sh
@@ -4,6 +4,7 @@ TOP_DIR=$(pwd)
 DIST_DIR=$(pwd)/dist
 CLIENT_SRC_DIR=$(pwd)/src/client/bibliograph
 SERVER_SRC_DIR=$(pwd)/src/server
+VERSION=$(node -p -e "require('$TOP_DIR/package.json').version")
 if [[ ! -d "$DIST_DIR" ]]; then
     echo "Cannot find 'dist' subdirectory - are you in the top folder?"
 fi
@@ -19,15 +20,19 @@ cp build-compiled/index.html $DIST_DIR
 cd $DIST_DIR/server
 cp -a $SERVER_SRC_DIR/{config,controllers,lib,messages,migrations,models,modules,schema} .
 rm config/{bibliograph.ini.php,console.php,message.php,test.php}
+rm runtime/cache/*
+rm runtime/logs/*
+
 cp -a $SERVER_SRC_DIR/composer.* .
 composer install --no-dev
 rm ./composer.*
 
 # Documentation
-cp $TOP_DIR/{readme.md,release-notes.md,package.json} $DIST_DIR
+cp $TOP_DIR/{readme.md,release-notes.md} $DIST_DIR
+echo $VERSION > $DIST_DIR/version.txt
 
 # package as zip
 cd $DIST_DIR
-zip -r bibliograph.zip *
+zip -r bibliograph-$VERSION.zip *
 
 
--- bin/clean-dist.sh
+++ bin/clean-dist.sh
@@ -5,9 +5,6 @@ if [[ ! -d "$DIST_DIR" ]]; then
     echo "Cannot find 'dist' subdirectory - are you in the top folder?"
 fi
 
-# Build files
-rm -rf src/client/bibliograph/build-compiled
-
 # dist - Client files
 rm -rf $DIST_DIR/{bibliograph,resource}
 rm $DIST_DIR/index.html
@@ -16,4 +13,4 @@ rm $DIST_DIR/index.html
 rm -rf $DIST_DIR/server/{config,controllers,lib,messages,migrations,models,modules,vendor,schema} 
 
 # documentation
-rm $DIST_DIR/{package.json,readme.md,release-notes.md}
+rm $DIST_DIR/{version.txt,readme.md,release-notes.md}
