22fd4ad88e946980a48f545b9914d58285b459e1
Christian M. Boulanger <info@bibliograph.org>
2018-04-19 12:24:48 +0200

    Allow dist builds in source mode

--- bin/build-dist.sh
+++ bin/build-dist.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
+BUILD_TARGET=source
 TOP_DIR=$(pwd)
 DIST_DIR=$(pwd)/dist
 CLIENT_SRC_DIR=$(pwd)/src/client/bibliograph
 SERVER_SRC_DIR=$(pwd)/src/server
 VERSION=$(node -p -e "require('$TOP_DIR/package.json').version")
+
 if [[ ! -d "$DIST_DIR" ]]; then
     echo "Cannot find 'dist' subdirectory - are you in the top folder?"
 fi
 
 # Client files
 cd $CLIENT_SRC_DIR
-qx compile --target=build
-cp -a build-compiled/bibliograph $DIST_DIR
-cp -a build-compiled/resource $DIST_DIR
-cp build-compiled/index.html $DIST_DIR
+qx compile --target=$BUILD_TARGET
+cp -a $BUILD_TARGET-compiled/bibliograph $DIST_DIR
+cp -a $BUILD_TARGET-compiled/resource $DIST_DIR
+cp $BUILD_TARGET-compiled/index.html $DIST_DIR
 
 # Server files
 cd $DIST_DIR/server
