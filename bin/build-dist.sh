2ec4aa350da32ae5bb5752b3343df35f30f9c105
Christian M. Boulanger <info@bibliograph.org>
2018-04-20 08:28:46 +0200

    Update release files

--- bin/build-dist.sh
+++ bin/build-dist.sh
@@ -1,41 +1,51 @@
 #!/usr/bin/env bash
 
-BUILD_TARGET=source
+BUILD_TARGET=${1:-build}
 TOP_DIR=$(pwd)
-DIST_DIR=$(pwd)/dist
-CLIENT_SRC_DIR=$(pwd)/src/client/bibliograph
-SERVER_SRC_DIR=$(pwd)/src/server
+DIST_DIR=$TOP_DIR/dist
+CLIENT_SRC_DIR=$TOP_DIR/src/client/bibliograph
+SERVER_SRC_DIR=$TOP_DIR/src/server
 VERSION=$(node -p -e "require('$TOP_DIR/package.json').version")
 
 if [[ ! -d "$DIST_DIR" ]]; then
     echo "Cannot find 'dist' subdirectory - are you in the top folder?"
 fi
 
-# Client files
+echo
+echo "Building distributable package of Bibliograph in '$BUILD_TARGET' mode"
+
+echo " >>> Building client ..."
 cd $CLIENT_SRC_DIR
-qx compile --target=$BUILD_TARGET
+qx compile --target=$BUILD_TARGET > /dev/null
 cp -a $BUILD_TARGET-compiled/bibliograph $DIST_DIR
 cp -a $BUILD_TARGET-compiled/resource $DIST_DIR
 [[ -d $BUILD_TARGET-compiled/transpiled ]] && cp -a $BUILD_TARGET-compiled/transpiled $DIST_DIR
 cp $BUILD_TARGET-compiled/index.html $DIST_DIR
 
-# Server files
+echo " >>> Building server ..."
 cd $DIST_DIR/server
-cp -a $SERVER_SRC_DIR/{config,controllers,lib,messages,migrations,models,modules,schema} .
+cp -a $SERVER_SRC_DIR/{config,controllers,lib,messages,migrations,models,modules,schema,runtime} .
+if [[ $BUILD_TARGET == "source" ]]; then
+  cp -a $SERVER_SRC_DIR/web .
+fi
+rm runtime/{cache,logs}/*
 rm config/{app.conf.toml,message.php,test.php}
-mkdir -p runtime/cache
-mkdir -p runtime/logs
 
 cp -a $SERVER_SRC_DIR/composer.* .
-composer install --no-dev
+if [[ $BUILD_TARGET == "source" ]]; then
+  composer install > /deve/null
+else 
+  composer install --no-dev  > /dev/null
+fi
 rm ./composer.*
 
-# Documentation
+echo " >>> Adding documentation ..."
 cp $TOP_DIR/{readme.md,release-notes.md} $DIST_DIR
 echo $VERSION > $DIST_DIR/version.txt
 
-# package as zip
+echo " >>> Creating ZIP file ..."
 cd $DIST_DIR
 zip -q -r bibliograph-$VERSION.zip *
 
+echo "Done."
 
