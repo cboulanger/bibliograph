8e085d9ef57fa83c0ac0b10b498a6fe39791c02f
Christian M. Boulanger <info@bibliograph.org>
2018-06-03 00:00:18 +0200

    Cast integer/boolen colums to NOT NULL

---
+++ src/server/modules/extendedfields/migrations/M180602120949_fix_integer_notnull_columns.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace app\modules\extendedfields\migrations;
+
+class M180602120949_fix_integer_notnull_columns
+  extends \app\migrations\schema\bibliograph_datasource\M180602120949_fix_integer_notnull_columns
+{}
