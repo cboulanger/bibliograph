88207d8f1278f17d58d4c0adc610b0852d4e8329
Christian Boulanger <info@bibliograph.org>
2018-06-03 18:33:37 +0200

    Release 3.0.0-beta.38

--- bin/github-tag-clean.sh
+++ bin/github-tag-clean.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+TOP_DIR=$(pwd)
+VERSION=$(node -p -e "require('$TOP_DIR/package.json').version")
 for tagName in $(git tag | grep 'alpha\|beta')
 do
-  git push --delete origin $tagName
-  git tag -d $tagName
+  if [[ "$tagName" != "$VERSION" ]]
+  then
+    git push --delete origin $tagName
+    git tag -d $tagName
+  fi
 done
