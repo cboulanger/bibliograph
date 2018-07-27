894e208ca7f37d8f2c084a3f512019d01b91ddfd
Christian Boulanger <info@bibliograph.org>
2018-07-27 22:14:23 +0200

    Switching from shared-git-hooks to git-hooks

--- .githooks/pre-push/pre-push.sh
+++ .githooks/pre-push/pre-push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-travis lint ./.travis.yml -x || exit 1
+echo "Pre-push hooks are disables"
+# travis lint ./.travis.yml -x || exit 1
 # shellcheck build/script/*.sh
 # shellcheck test/script/*.sh
