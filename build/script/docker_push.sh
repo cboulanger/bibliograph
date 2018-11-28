3fe2f7334251635f049f74accbc1ec761b62050d
Christian Boulanger <info@bibliograph.org>
2018-11-28 17:00:42 +0100

    Fixed docker build script to only execute in the PHP 7.2 run

--- build/script/docker_push.sh
+++ build/script/docker_push.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# Generates a docker image and pushes it to docker hub
+PHP_TARGET_VERSION=7.2
+PHP_VERSION=$(php -r "echo substr(phpversion(),0,strrpos(phpversion(),'.'));")
+if [[ "$PHP_VERSION" != "$PHP_TARGET_VERSION" ]]; then
+  echo "Not generating docker image for PHP $PHP_VERSION"
+  exit 0
+fi
+echo "Generating docker image for PHP $PHP_VERSION...p"
 echo " >>> reverting changes to local repo ..."
 git checkout .
 REPO=cboulanger/bibliograph
