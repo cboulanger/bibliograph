ef816cd0166c23a1b5865ecc46581b9f2715e1f0
Christian Boulanger <info@bibliograph.org>
2018-11-28 11:11:25 +0100

    Fix travis docker scripts

--- install/docker/docker_push.sh
+++ install/docker/docker_push.sh
@@ -4,7 +4,7 @@ git checkout .
 REPO=cboulanger/bibliograph
 TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 echo " >>> Building image $REPO:$TAG' ..."
-docker build -f ./build/env/docker/Dockerfile -t $REPO:$TAG .
+docker build -f ./install/docker/Dockerfile -t $REPO:$TAG .
 docker tag $REPO:$TAG $REPO:$(git describe --tags)
 echo " >>> Pushing to Docker hub...."
 echo "$DOCKER_PASSWORD" | docker login -u cboulanger --password-stdin
