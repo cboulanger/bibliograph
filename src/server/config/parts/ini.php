871df295eea88a123d87e296147fa9a851dccfcc
Christian Boulanger <info@bibliograph.org>
2019-02-17 14:44:03 +0100

    Fixing boot issues, testing travis stages

--- src/server/config/parts/ini.php
+++ src/server/config/parts/ini.php
@@ -1,3 +1,9 @@
 <?php
 use Yosymfony\Toml\Toml;
+if (!method_exists("Toml", "parseFile")) {
+  // v0.x, as long as we support PHP7.0
+  // @todo remove after PHP7.0 support is dropped
+  return Toml::parse(file_get_contents(APP_CONFIG_FILE));
+}
+// >v1.0
 return Toml::parseFile(APP_CONFIG_FILE);
