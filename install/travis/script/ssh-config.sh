05eef75a95833f463af141f2cf6238250151750c
Christian Boulanger <info@bibliograph.org>
2019-02-19 23:05:46 +0100

    Reorganize travis scripts (skip tests)

--- install/travis/script/ssh-config.sh
+++ install/travis/script/ssh-config.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # decrypt and use deployment SSH config
-openssl aes-256-cbc -K $encrypted_f52210703301_key -iv $encrypted_f52210703301_iv -in install/travis/deploy_secrets.tar.enc -out deploy_secrets.tar -d
+openssl aes-256-cbc \
+  -K $encrypted_f52210703301_key \
+  -iv $encrypted_f52210703301_iv \
+  -in install/travis/deploy_secrets.tar.enc \
+  -out deploy_secrets.tar -d
 tar xvf deploy_secrets.tar
 eval "$(ssh-agent -s)"
 mv ./deploy_rsa ~/.ssh/
