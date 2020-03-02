a2659b1985c8e2bafb95df6b7b31b3a520e848a7
Christian Boulanger <info@bibliograph.org>
2020-03-02 08:19:07 +0100

    Remove PHP7.0 support

--- tool/install/travis/script/install-php.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-PHPVERSION=$(phpenv version-name)
-sudo add-apt-repository -y ppa:ondrej/php && sudo apt-get update
-sudo apt-get install -y php${PHPVERSION}-xsl php${PHPVERSION}-intl
