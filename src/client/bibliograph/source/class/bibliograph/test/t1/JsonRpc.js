a257640c2609b59fa10547e2f3de5364b58bab10
Christian Boulanger <info@bibliograph.org>
2020-05-01 22:35:01 +0200

    Refactored tooling

--- src/client/bibliograph/source/class/bibliograph/test/t1/JsonRpc.js
+++ src/client/bibliograph/source/class/bibliograph/test/t1/JsonRpc.js
@@ -1,7 +1,7 @@
 /**
  * @require(qx.io.jsonrpc.transport.Http)
  */
-qx.Class.define("bibliograph.test.JsonRpc", {
+qx.Class.define("bibliograph.test.t1.JsonRpc", {
   extend: qx.dev.unit.TestCase,
   include: [
     qx.test.io.jsonrpc.MAssert
@@ -10,20 +10,20 @@ qx.Class.define("bibliograph.test.JsonRpc", {
     /**
      * @var {qcl.io.jsonrpc.Client}
      */
-    __client: null,
+    client: null,
     
     setUp () {
       let url =`${location.protocol}//${location.host}/${qx.core.Environment.get("app.serverUrl")}/json-rpc`;
-      this.__client = new qcl.io.jsonrpc.Client(url, "json-rpc-test");
+      this.client = new qcl.io.jsonrpc.Client(url, "json-rpc-test");
     },
     
     tearDown() {
-      this.__client.dispose();
+      this.client.dispose();
     },
   
     async "test: receive a notification from the server"() {
       let value = Math.PI;
-      await this.__client.request("notify-me", [value]);
+      await this.client.request("notify-me", [value]);
       this.wait(100, () => {
         this.assertEquals(value, bibliograph.test.notification_received);
       });
@@ -31,31 +31,31 @@ qx.Class.define("bibliograph.test.JsonRpc", {
     
     async "test: echo an Array value"() {
       let value = [1, 2, 3];
-      let result = await this.__client.request("echo-array", [value]);
+      let result = await this.client.request("echo-array", [value]);
       this.assertDeepEquals(value, result);
     },
     
     async "test: echo a String value"() {
       let value = "foo";
-      let result = await this.__client.request("echo", [value]);
+      let result = await this.client.request("echo", [value]);
       this.assertEquals(value, result);
     },
     
     async "test: echo an Integer value"() {
       let value = 1;
-      let result = await this.__client.request("echo", [value]);
+      let result = await this.client.request("echo", [value]);
       this.assertEquals(value, result);
     },
     
     async "test: echo a Float value"() {
       let value = Math.PI;
-      let result = await this.__client.request("echo", [value]);
+      let result = await this.client.request("echo", [value]);
       this.assertEquals(value, result);
     },
     
     async "test: echo an Object value"() {
       let value = {"a": "foo", b: 1, c: [1, 2, 3]};
-      let result = await this.__client.request("echo", [value]);
+      let result = await this.client.request("echo", [value]);
       this.assertDeepEquals(value, result);
     },
     
---
+++ src/client/bibliograph/source/class/bibliograph/test/t2/Access.js
@@ -0,0 +1,75 @@
+/**
+ * @require(qx.io.jsonrpc.transport.Http)
+ */
+qx.Class.define("bibliograph.test.t2.Access", {
+  extend: qx.dev.unit.TestCase,
+  include: [
+    qx.test.io.jsonrpc.MAssert
+  ],
+  members: {
+    /**
+     * @var {qcl.io.jsonrpc.Client}
+     */
+    client: null,
+    
+    setUp () {
+      let url =`${location.protocol}//${location.host}/${qx.core.Environment.get("app.serverUrl")}/json-rpc`;
+      this.client = new qcl.io.jsonrpc.Client(url);
+      this.client.setErrorBehavior("debug");
+    },
+    
+    tearDown() {
+      this.client.dispose();
+    },
+    
+    async loginAnonymously() {
+      let result = await this.client.request("access.authenticate");
+      this.assertString(result.token);
+      this.assertNotEquals("", result.token);
+      this.client.setToken(result.token);
+    },
+  
+    async loginWithPassword(username, password) {
+      let result = await this.client.request("access.authenticate", [username, password]);
+      this.assertString(result.token);
+      this.assertNotEquals("", result.token);
+      this.client.setToken(result.token);
+    },
+    
+    async "test: try to access method without authentication - should fail"() {
+      try {
+        await this.client.request("access.username");
+        throw new Error("Unauthenticated access should throw");
+      } catch (e) {
+        this.assertInstance(e, qx.io.jsonrpc.exception.JsonRpc);
+        this.assertEquals("Unauthorized: Your request was made with invalid credentials.", e.message);
+      }
+    },
+    
+    async "test: log in anonymously and get username"() {
+      await this.loginAnonymously();
+      let username = await this.client.request("access.username");
+      console.log(`Username is ${username}.`);
+      this.assertString(username);
+    },
+  
+    async "test: log in anonymously and test persistence"() {
+      await this.loginAnonymously();
+      for (let i=1; i<4; i++) {
+        this.assertEquals(i, await this.client.request("access.count"));
+      }
+    },
+    
+    async "test: authenticate with Password"() {
+      await this.loginWithPassword("admin", "admin");
+      for (let i=1; i<4; i++) {
+        this.assertEquals(i, await this.client.request("access.count"));
+      }
+      let result = await this.client.request("access.userdata");
+      this.assertEquals("admin", result.namedId);
+      this.assertInArray("*", result.permissions);
+    },
+    
+    eof() {}
+  }
+});
