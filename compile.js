33138d6f7b30dd991dcbc397469c729b72d358f8
Christian Boulanger <info@bibliograph.org>
2020-09-23 22:59:37 +0200

    next try 65

--- compile.js
+++ compile.js
@@ -1,21 +1,22 @@
 /**
  * Runs UI tests for Bibliograph
  */
-const fs = require("fs");
-const path = require("path");
-const TEST_PATH = path.join("test", "uitests");
+// const path = require("path");
+//const TEST_PATH = path.join("test", "uitests");
 
 qx.Class.define("bibliograph.CompilerApi", {
   extend: qx.tool.cli.api.CompilerApi,
   statics: {
-    TEST_PATH
+//    TEST_PATH
   },
   members: {
-  
-    beforeTests(command) {
-      command.addTest(new qx.tool.cli.api.Test("fake test", async function() {
-        console.log("# This is a fake test");
-        this.setExitCode(0);
+    
+    async beforeTests(command) {
+      command.addTest(new qx.tool.cli.api.Test("Browser tests", async function() {
+        let app = this.getTestApp("qxl.testtapper.Application");
+        let result = {};
+        await this.runTest(app, result);
+        this.setExitCode(result.errorCode);
       })).setNeedsServer(false);
     },
 
@@ -23,25 +24,101 @@ qx.Class.define("bibliograph.CompilerApi", {
       //
     },
   
-    /**
-     * Run the javascript-based tests
-     * @param command
-     * @return {Promise<void>}
-     */
-    // async __runTests(command) {
-    //   let files =
-    //         fs.readdirSync(this.constructor.TEST_PATH)
-    //         .filter(file => fs.statSync(path.join(this.constructor.TEST_PATH, file)).isFile());
-    //   for (let file of files) {
-    //     let test = path.changeExt(path.basename(file), "");
-    //     command.addTest(new qx.tool.cli.api.Test(test, async () => {
-    //       let result = await qx.tool.utis.Utils.runCommand(this.constructor.TEST_PATH, "node", test + ".js");
-    //       this.setExitCode(result.exitCode);
-    //     })).setNeedsServer(false);
-    //   }
-    // },
-    
-    eof() {}
+    runTest : async function (app, result) {
+      return new qx.Promise(async resolve => {
+        const puppeteer = this.require("puppeteer");
+        const pti = this.require("puppeteer-to-istanbul");
+        let outputDir = "";
+        if (this.getCompilerApi().getCommand().showStartpage()) {
+          let target = app.maker.getTarget();
+          outputDir = target.getOutputDir();
+        }
+        let href = `http://localhost:${app.port}/${outputDir}${app.name}/`;
+        const { URL } = require("url");
+        let url = new URL(href);
+        let s = "";
+        if (app.argv.method) {
+          s += "method=" + app.argv.method;
+        }
+        if (app.argv.class) {
+          if (s.length > 0) {
+            s += "&";
+          }
+          s += "class=" + app.argv.class;
+        }
+        if (s.length > 0) {
+          url.search = s;
+        }
+        qx.tool.compiler.Console.log("CALL "+ url.href);
+        let notOk = 0;
+        let Ok = 0;
+        let browser = await puppeteer.launch();
+        let page = await browser.newPage();
+        page.on("console", async (msg) => {
+          let val = msg.text();
+          // value is serializable
+          if  (val.match(/^\d+\.\.\d+$/)) {
+            qx.tool.compiler.Console.info(`DONE testing ${Ok} ok, ${notOk} not ok`);
+            let jsCoverage = await page.coverage.stopJSCoverage();
+            qx.tool.compiler.Console.info("writing coverage information ...");
+            await pti.write(jsCoverage);
+            await browser.close();
+            result.errorCode += notOk;
+            result[app.name] = {
+              notOk: notOk,
+              ok: Ok
+            };
+            resolve(result[app.name]);
+          } else if (val.match(/^not ok /)) {
+            notOk++;
+            qx.tool.compiler.Console.log(val);
+          } else if (val.match(/^ok\s/)) {
+            Ok++;
+            if (!app.argv.terse) {
+              qx.tool.compiler.Console.log(val);
+            }
+          } else if (val.match(/^#/) && app.argv.diag) {
+            qx.tool.compiler.Console.log(val);
+          } else if (app.argv.verbose) {
+            qx.tool.compiler.Console.log(val);
+          }
+        });
+        await page.coverage.startJSCoverage();
+        await page.goto(url.href);
+      });
+    },
+  
+    getTestApp: function(classname) {
+      let command = this.getCompilerApi().getCommand();
+      let maker = null;
+      let app = null;
+      command.getMakers().forEach(tmp => {
+        let apps = tmp.getApplications().filter(app => (app.getClassName() === classname) && app.isBrowserApp());
+        if (apps.length) {
+          if (maker) {
+            qx.tool.compiler.Console.print("qx.tool.cli.test.tooManyMakers");
+            return null;
+          }
+          if (apps.length !== 1) {
+            qx.tool.compiler.Console.print("qx.tool.cli.test.tooManyApplications");
+            return null;
+          }
+          maker = tmp;
+          app = apps[0];
+        }
+      });
+      if (!app) {
+        qx.tool.compiler.Console.print("qx.tool.cli.test.noAppName");
+        return null;
+      }
+      var config = command._getConfig();
+      return {
+        name: app.getName(),
+        port: config.serve.listenPort,
+        argv: command.argv,
+        maker: maker
+      };
+    }
   }
 });
 
