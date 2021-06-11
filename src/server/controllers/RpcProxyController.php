33ce22bbdf05adf066c0b9cd91e88f49de0b737f
Christian Boulanger <info@bibliograph.org>
2021-06-11 15:21:38 +0200

    Add missing const keyword

--- src/server/controllers/RpcProxyController.php
+++ src/server/controllers/RpcProxyController.php
@@ -18,7 +18,7 @@ class RpcProxyController extends \yii\console\Controller
     'do', 'if', 'in', 'for', 'let', 'new', 'try', 'var', 'case', 'else', 'enum', 'eval', 'null', 'this', 'true', 'void',
     'with', 'await', 'break', 'catch', 'class', 'const', 'false', 'super', 'throw', 'while', 'yield', 'delete', 
     'export', 'import', 'public', 'return', 'static', 'switch', 'typeof', 'default', 'extends', 'finally', 'package',
-    'private', 'continue', 'debugger', 'function', 'arguments', 'interface', 'protected', 'implements', 'instanceof'
+    'private', 'continue', 'debugger', 'function', 'arguments', 'interface', 'protected', 'implements', 'instanceof', 'const'
   ];
 
 
